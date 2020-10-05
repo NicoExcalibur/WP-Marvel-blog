be16b69f5ba9a0e8774db4f72194c655f15c0f09
NicoExcalibur <nico.zanz@gmail.com>
2020-10-05 16:59:35 +0200

    :sparkles:add twitter widget on home

--- content/themes/marvelblog/index.php
+++ content/themes/marvelblog/index.php
@@ -1,5 +1,7 @@
 <?php get_header(); ?>
-    
+ 
+<?php get_template_part('template-parts/twitter'); ?>
+
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 <?php get_template_part('template-parts/post-excerpt'); ?>
---
+++ content/themes/marvelblog/template-parts/twitter.php
@@ -0,0 +1,2 @@
+<a class="twitter-timeline" data-lang="fr" data-width="300" data-height="500" data-theme="light" href="https://twitter.com/Marvel?ref_src=twsrc%5Etfw">Tweets du compte officiel Marvel</a> 
+<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
