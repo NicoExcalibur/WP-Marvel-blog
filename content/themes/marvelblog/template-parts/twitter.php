91d7739d3c24bab0fd44309c37317f0c735a0a64
NicoExcalibur <nico.zanz@gmail.com>
2020-10-05 17:14:40 +0200

    :art:improve code structure in scss folder

--- content/themes/marvelblog/template-parts/twitter.php
+++ content/themes/marvelblog/template-parts/twitter.php
@@ -1,2 +1,6 @@
-<a class="twitter-timeline" data-lang="fr" data-width="300" data-height="500" data-theme="light" href="https://twitter.com/Marvel?ref_src=twsrc%5Etfw">Tweets du compte officiel Marvel</a> 
-<script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+<div class="twitter">
+    <a class="twitter-timeline" data-lang="fr" data-width="250" data-height="500" data-theme="light" href="https://twitter.com/Marvel?ref_src=twsrc%5Etfw">
+        Tweets du compte officiel Marvel
+    </a>
+    <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+</div>
